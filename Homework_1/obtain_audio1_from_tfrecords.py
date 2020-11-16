c6639b18e89a7abf6fb9698e98b8e2d926dc6590
usesnames <56475319+usesnames@users.noreply.github.com>
2020-11-16 16:19:38 +0100

    now extracts also posix date

--- Homework_1/obtain_audio1_from_tfrecords.py
+++ Homework_1/obtain_audio1_from_tfrecords.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-raw_dataset = tf.data.TFRecordDataset("output") #output is the name you gave to the tfrecords file 
+raw_dataset = tf.data.TFRecordDataset("output2")
 
 def read_tfrecord(serialized_example):
   feature_description = {
@@ -11,11 +11,13 @@ def read_tfrecord(serialized_example):
   example = tf.io.parse_single_example(serialized_example, feature_description)
   
   audio = example['audio']
+  datetime = example['datetime']
   
-  return audio
+  return audio, datetime
 
 parsed_dataset = raw_dataset.map(read_tfrecord)
 
 for data in parsed_dataset.take(1):
-  tf.io.write_file("sperem.wav", data)
+  tf.io.write_file("sperem.wav", data[0])
+  print(data[1])
 
