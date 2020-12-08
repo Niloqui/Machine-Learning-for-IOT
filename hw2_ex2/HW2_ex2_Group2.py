e5f01414943c846d0dca3eec86f49d519d95432e
Alessandro Nicolì <38593853+Niloqui@users.noreply.github.com>
2020-12-08 01:07:12 +0100

    Update HW2_ex2_Group2.py

--- hw2_ex2/HW2_ex2_Group2.py
+++ hw2_ex2/HW2_ex2_Group2.py
@@ -6,10 +6,12 @@ import numpy as np
 import pandas as pd
 import zlib
 
+import tensorflow_model_optimization as tfmot
 import tensorflow as tf
 import tensorflow.lite as tflite
 #from tensorflow import keras
-import tf.keras as keras
+keras = tf.keras
+
 
 # Reading arguments
 parser = argparse.ArgumentParser()
@@ -20,10 +22,12 @@ args = parser.parse_args()
 mfccs = args.mfccs
 force_first_train = args.force_first_train
 
+
 # Setting seed for random number generation
 seed = 42
 tf.random.set_seed(seed)
 np.random.seed(seed)
+
 
 # Loading the dataset
 data_dir = os.path.join('.','data', 'mini_speech_commands')
@@ -42,7 +46,8 @@ class SignalGenerator:
                  lower_freq=20, upper_freq=48000, num_coefficients=10, mfccs=False):
         self.frame_length = frame_length
         self.frame_step = frame_step
-        self.mel_inputs =  [num_mel_bins, None, sampling_rate, lower_freq, upper_freq]
+        self.num_mel_bins = num_mel_bins
+        self.mel_inputs = [num_mel_bins, None, sampling_rate, lower_freq, upper_freq]
         self.mfccs_coeff = num_coefficients
         self.labels=labels
         self.sampling_rate=sampling_rate
@@ -79,8 +84,8 @@ class SignalGenerator:
     def get_mfcc(self, spectrogram):
         mel_spectrogram = tf.tensordot(spectrogram, self.l2mel_matrix, 1)
         log_mel_spectrogram = tf.math.log(mel_spectrogram + 1e-6)
-        mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)[..., :self.mfccs_coeff]
-        return mfccs
+        mfccs_ = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)[..., :self.mfccs_coeff]
+        return mfccs_
 
     def preprocess_with_stft(self, file_path):
         audio, label = self.read(file_path)
@@ -94,9 +99,10 @@ class SignalGenerator:
         audio, label = self.read(file_path)
         audio = self.pad(audio)
         spectrogram = self.get_spectrogram(audio)
-        spectrogram = tf.expand_dims(spectrogram, -1)
-        mfccs = self.get_mfcc(spectrogram)
-        return mfccs, label
+        #spectrogram = tf.expand_dims(spectrogram, -1)
+        mfccs_ = self.get_mfcc(spectrogram)
+        mfccs_ = tf.expand_dims(mfccs_, -1)
+        return mfccs_, label
 
     def make_dataset(self, files, train):
         ds = tf.data.Dataset.from_tensor_slices(files)
@@ -109,10 +115,9 @@ class SignalGenerator:
 
         return ds
 
-STFT_OPTIONS = {'frame_length': 256, 'frame_step': 128, 'mfcc': False}
-MFCC_OPTIONS = {'frame_length': 640, 'frame_step': 320, 'mfcc': True,
-        'lower_frequency': 20, 'upper_frequency': 4000, 'num_mel_bins': 40,
-        'num_coefficients': 10}
+STFT_OPTIONS = {'frame_length': 256, 'frame_step': 128, 'mfccs': False}
+MFCC_OPTIONS = {'frame_length': 640, 'frame_step': 320, 'mfccs': True,
+        'lower_freq': 20, 'upper_freq': 4000, 'num_mel_bins': 40, 'num_coefficients': 10}
 
 if mfccs:
     options = MFCC_OPTIONS
@@ -121,9 +126,19 @@ else:
     options = STFT_OPTIONS
     stride = [2, 2]
 
-dataset_dir = 'data/mini_speech_commands_datasets'
+dataset_dir = 'data/mini_speech_commands' + ("_mfccs" if mfccs else "_stft")
 if os.path.exists(dataset_dir):
-    tensor_specs = (tf.TensorSpec([None,32,32,1],dtype=tf.float32),tf.TensorSpec([None,],dtype=tf.int64))
+    if mfccs:
+        tensor_specs = (
+            tf.TensorSpec([None,49,10,1],dtype=tf.float32),
+            tf.TensorSpec([None,],dtype=tf.int64)
+        )
+    else: # stft
+        tensor_specs = (
+            tf.TensorSpec([None,32,32,1],dtype=tf.float32),
+            tf.TensorSpec([None,],dtype=tf.int64)
+        )
+    
     train_ds = tf.data.experimental.load(f'{dataset_dir}/th_train', tensor_specs)
     val_ds = tf.data.experimental.load(f'{dataset_dir}/th_val', tensor_specs)
     test_ds = tf.data.experimental.load(f'{dataset_dir}/th_test', tensor_specs)
@@ -141,6 +156,7 @@ else:
     tf.data.experimental.save(train_ds, f'{dataset_dir}/th_train')
     tf.data.experimental.save(val_ds, f'{dataset_dir}/th_val')
     tf.data.experimental.save(test_ds, f'{dataset_dir}/th_test')
+
 
 # Model definition
 models = {
@@ -181,7 +197,7 @@ models = {
     ])
 }
 
-# Training the first
+# Training for the first time
 for i, model_name in enumerate(models):
     callback_folder_name = f'./callback_test_chkp/{model_name}{"_mfccs" if mfccs else "_stft"}_chkp_best'
     
@@ -190,12 +206,12 @@ for i, model_name in enumerate(models):
         model.compile(
             optimizer = 'Adam',
             loss = keras.losses.SparseCategoricalCrossentropy(True),
-            metrics = tf.keras.metrics.SparseCategoricalAccuracy()
+            metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
         )
         
         cp_callback = keras.callbacks.ModelCheckpoint(
             callback_folder_name,
-            monitor = 'val_loss',
+            monitor = 'val_sparse_categorical_accuracy',
             verbose = 0, 
             save_best_only = True,
             save_weights_only = False,
@@ -213,7 +229,8 @@ for i, model_name in enumerate(models):
         
         msize = os.path.getsize(f'{callback_folder_name}/saved_model.pb')
         print(f'\nacc: {test_acc}, size: {msize} Inference Latency {end}ms\n')
-
+    else:
+        print(f'{callback_folder_name} already exists.')
 
 
 # Saving basic tflite model
@@ -230,62 +247,148 @@ for model_name in models:
     compressed_file = f'{tflite_dirs}/{model_name}{"_mfccs" if mfccs else "_stft"}_compressed.tflite.zlib'
     
     # Basic file
-    converter = tf.lite.TFLiteConverter.from_saved_model(model_folder)
-    tflite_model = converter.convert()
+    if force_first_train or not os.path.exists(basic_file):
+        with open(basic_file, 'wb') as f:
+            converter = tf.lite.TFLiteConverter.from_saved_model(model_folder)
+            tflite_model = converter.convert()
+            
+            f.write(tflite_model)
     
-    with open(basic_file, 'wb') as f:
-        f.write(tflite_model)
-    
-    tflb_size = os.path.getsize(tflite_model)
-    print(f'Size of basic model: {tflb_size/1024} kB')
+    tflb_size = os.path.getsize(basic_file)
     
     # Optimized file
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    tflite_quant_model = converter.convert()
-    
-    with open(optimized_file, 'wb') as f:
-        f.write(tflite_quant_model)
+    if force_first_train or not os.path.exists(optimized_file):
+        with open(optimized_file, 'wb') as f:
+            converter.optimizations = [tf.lite.Optimize.DEFAULT]
+            tflite_quant_model = converter.convert()
+            
+            f.write(tflite_quant_model)
     
     tflo_size = os.path.getsize(optimized_file)
-    print(f'Size of optimized model: {tflo_size/1024}kB')
     
     # Compressed file
-    with open(compressed_file, 'wb') as fp:
-        tflite_compressed = zlib.compress(tflite_quant_model)
-        fp.write(tflite_compressed)
+    if force_first_train or not os.path.exists(compressed_file):
+        with open(compressed_file, 'wb') as fp:
+            tflite_compressed = zlib.compress(tflite_quant_model)
+            fp.write(tflite_compressed)
     
     tflc_size=os.path.getsize(compressed_file)
-    print(f'\nCompressed: {tflc_size/1024}kB')
     
     
-    ###### ASD
-    '''
-    interpreter = tflite.Interpreter(model_path = tflite_dirs+f"/{mod}{'_mfccs' if mfccs else ''}.tflite")
+    # Evaluating the model
+    interpreter = tflite.Interpreter(model_path = optimized_file)
     interpreter.allocate_tensors()
-    # Get input and output tensors.
+    
+    # Get input and output tensors
     input_details = interpreter.get_input_details()
-    print('input: ', input_details[0]['dtype'])
-    output_details = interpreter.get_output_details()
-    print('output: ', output_details[0]['dtype'])
-
     input_shape = input_details[0]['shape']
+    output_details = interpreter.get_output_details()
+    
     num_corr = 0
     num = 0
     start = t.time()
-    for input_data,label in test_ds:
+    for input_data, label in test_ds:
         #input_data = convert(input_data, 0, 255, np.uint8)
         #label = convert(label, 0, 255, np.uint8)
         #input_data = tf.quantization.quantize(input_data,min(input_data),max(input_data),tf.quint8)
         #label = tf.quantization.quantize(label,min(LABELS),max(LABELS),tf.quint8)
-        num += 1
+        
         interpreter.set_tensor(input_details[0]['index'], input_data)
         interpreter.invoke()
         output_data = np.argmax(interpreter.get_tensor(output_details[0]['index']))
-        y_pred = tf.constant([output_data],dtype=tf.int64)
+        #y_pred = tf.constant([output_data],dtype=tf.int64)
+        
         if label.numpy()[0] == output_data:
-            num_corr+=1
+            num_corr += 1
+        num += 1
+    
     end = t.time() - start
-    print(f'accuracy: {num_corr/num} tflite size: {tfl_size/1024}kB compressed: {tflc_size/1024}kB time: {end}ms')
+    
+    # Final outputs
+    print(f'Size of basic model: {tflb_size/1024} kB')
+    print(f'Size of optimized model: {tflo_size/1024} kB')
+    print(f'Compressed: {tflc_size/1024} kB')
+    print(f'Accuracy: {num_corr/num}\nTime: {end} ms\n\n')
+
+
+# Stripping the model
+pruning_params = {
+    'pruning_schedule' : tfmot.sparsity.keras.PolynomialDecay(
+        initial_sparsity=0.30, 
+        final_sparsity=0.8,
+        begin_step=len(train_ds)*5,
+        end_step=len(train_ds)*15
+    )
+}
+prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
+
+for model_name in models:
+    model_folder = f'./callback_test_chkp/{model_name}{"_mfccs" if mfccs else "_stft"}_chkp_best'
+    stripped_model_folder = f'./stripped/{model_name}{"_mfccs" if mfccs else "_stft"}_chkp_best'
+    
+    if force_first_train or not os.path.exists(stripped_model_folder) or True:
+        model = tf.keras.models.load_model(model_folder)
+        model = prune_low_magnitude(model, **pruning_params)
+        
+        cp_callback = keras.callbacks.ModelCheckpoint(
+            f'./pruned/dscnn_chkp_best_mfccs',
+            monitor='val_loss',
+            verbose=0, 
+            save_best_only=True,
+            save_weights_only=False,
+            mode='auto',
+            save_freq='epoch'
+        )
+        callbacks = [tfmot.sparsity.keras.UpdatePruningStep(), cp_callback]
+        
+        input_shape = [32, 32, 32]
+        model.build(input_shape)
+        model.compile(
+            optimizer = 'Adam',
+            loss = keras.losses.SparseCategoricalCrossentropy(True),
+            metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
+        )
+        
+        model.fit(train_ds, epochs=20, validation_data=val_ds, callbacks=callbacks)
+        strip_model = tfmot.sparsity.keras.strip_pruning(model)
+        strip_model.save(stripped_model_folder)
+        strip_model.summary()
+    
+    """
+    # Evaluating the model
+    interpreter = tflite.Interpreter(model_path = stripped_model_folder)
+    interpreter.allocate_tensors()
+    
+    # Get input and output tensors
+    input_details = interpreter.get_input_details()
+    input_shape = input_details[0]['shape']
+    output_details = interpreter.get_output_details()
+    
+    num_corr = 0
+    num = 0
+    start = t.time()
+    for input_data, label in test_ds:
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+        output_data = np.argmax(interpreter.get_tensor(output_details[0]['index']))
+        
+        if label.numpy()[0] == output_data:
+            num_corr += 1
+        num += 1
+    end = t.time() - start
+    
+    msize = os.path.getsize(f'{stripped_model_folder}/saved_model.pb')
+    print(f'\nacc: {test_acc}, size: {msize} Inference Latency {end}ms\n')
+    
+    """
+    '''
+    start = t.time()
+    test_loss, test_acc2 = model.evaluate(test_ds, verbose=2)
+    end = t.time() - start
+    msize = os.path.getsize(f'./pruned/dscnn_chkp_best/saved_model.pb')
+    print()
+    print(f'acc: {test_acc2}, size: {msize} Inference Latency {end}ms')
+    print()
     '''
 
 
@@ -298,3 +401,15 @@ for model_name in models:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+print("End.")
