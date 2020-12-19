951d0fd9f87433c59dd5a49f488b36efa284b144
Alessandro Nicolì <38593853+Niloqui@users.noreply.github.com>
2020-12-19 15:59:17 +0100

    Update HW2_ex2_Group2.py

--- hw2_ex2/HW2_ex2_Group2.py
+++ hw2_ex2/HW2_ex2_Group2.py
@@ -13,23 +13,25 @@ import tensorflow.lite as tflite
 keras = tf.keras
 
 
-# Reading arguments
+### Reading arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--mfccs', action='store_true', help='Use MFCCs')
+#parser.add_argument('--mfccs', action='store_true', help='Use MFCCs')
+parser.add_argument('--stft', action='store_true', help='Use STFT insead of MFCCs')
+parser.add_argument('--seed', default=42, help='Set initial seed')
 parser.add_argument('--force-first-train', action='store_true', help='Overwrite original models')
 args = parser.parse_args()
 
-mfccs = args.mfccs
+mfccs = not args.stft
 force_first_train = args.force_first_train
 
 
-# Setting seed for random number generation
-seed = 42
+### Setting seed for random number generation
+seed = args.seed
 tf.random.set_seed(seed)
 np.random.seed(seed)
 
 
-# Loading the dataset
+### Loading the dataset
 data_dir = os.path.join('.','data', 'mini_speech_commands')
 if not os.path.exists(data_dir):
     zip_path = keras.utils.get_file(
@@ -158,100 +160,106 @@ else:
     tf.data.experimental.save(test_ds, f'{dataset_dir}/th_test')
 
 
-# Model definition
-models = {
-    'MLP' : keras.Sequential([
-        keras.layers.Flatten(),
-        keras.layers.Dense(units=256,activation=keras.activations.relu),
-        keras.layers.Dense(units=256,activation=keras.activations.relu),
-        keras.layers.Dense(units=256,activation=keras.activations.relu),
-        keras.layers.Dense(units=len(LABELS))
-    ]),
-    'CNN' : keras.Sequential([
-        keras.layers.Conv2D(filters=128, kernel_size=[3,3], strides = stride, use_bias=False),
-        keras.layers.BatchNormalization(momentum=0.1),
-        keras.layers.ReLU(),
-        keras.layers.Conv2D(filters=128, kernel_size=[3,3], strides = stride, use_bias=False),
-        keras.layers.BatchNormalization(momentum=0.1),
-        keras.layers.ReLU(),
-        keras.layers.Conv2D(filters=128, kernel_size=[3,3], strides = stride, use_bias=False),
-        keras.layers.BatchNormalization(momentum=0.1),
-        keras.layers.ReLU(),
-        keras.layers.GlobalAveragePooling2D(),
-        keras.layers.Dense(units=len(LABELS))
-    ]),
-    'DSCNN' : keras.Sequential([
+### Model definition
+# A modified version of the DSCNN
+model = keras.Sequential([
         keras.layers.Conv2D(filters=256, kernel_size=[3, 3], strides=stride, use_bias=False),
         keras.layers.BatchNormalization(momentum=0.1),
+        keras.layers.Dropout(0.1),
         keras.layers.ReLU(),
         keras.layers.DepthwiseConv2D(kernel_size=[3, 3], strides=[1, 1], use_bias=False),
         keras.layers.Conv2D(filters=256, kernel_size=[1, 1], strides=[1, 1], use_bias=False),
         keras.layers.BatchNormalization(momentum=0.1),
+        keras.layers.Dropout(0.1),
         keras.layers.ReLU(),
         keras.layers.DepthwiseConv2D(kernel_size=[3, 3], strides=[1, 1], use_bias=False),
         keras.layers.Conv2D(filters=256, kernel_size=[1, 1], strides=[1, 1], use_bias=False),
         keras.layers.BatchNormalization(momentum=0.1),
+        keras.layers.Dropout(0.1),
         keras.layers.ReLU(),
         keras.layers.GlobalAveragePooling2D(),
+        keras.layers.Dropout(0.5),
         keras.layers.Dense(units=len(LABELS))
-    ])
-}
+])
 
-# Training for the first time
-for i, model_name in enumerate(models):
-    callback_folder_name = f'./callback_test_chkp/{model_name}{"_mfccs" if mfccs else "_stft"}_chkp_best'
+width_scaling = 0.5
+original_filters = model.layers[0].filters
+model.layers[0].filters=int(width_scaling * original_filters)
+model.layers[5].filters=int(width_scaling * original_filters)
+model.layers[10].filters=int(width_scaling * original_filters)
+
+
+### Training for the first time
+callback_folder_name = f'./callback_test_chkp/model{"_mfccs" if mfccs else "_stft"}_chkp_best'
+
+if force_first_train or not os.path.exists(callback_folder_name):
+    model.compile(
+        optimizer = 'Adam',
+        loss = keras.losses.SparseCategoricalCrossentropy(True),
+        metrics = [keras.metrics.SparseCategoricalAccuracy()]
+    )
     
-    if force_first_train or not os.path.exists(callback_folder_name):
-        model = models[model_name]
-        model.compile(
-            optimizer = 'Adam',
-            loss = keras.losses.SparseCategoricalCrossentropy(True),
-            metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
+    cp_callback = keras.callbacks.ModelCheckpoint(
+        callback_folder_name,
+        monitor = 'val_sparse_categorical_accuracy',
+        verbose = 0, 
+        save_best_only = True,
+        save_weights_only = False,
+        mode = 'auto',
+        save_freq = 'epoch'
+    )
+    
+    model.fit(train_ds, batch_size=32, epochs=20, validation_data=val_ds,
+            callbacks=[cp_callback])
+    model.summary()
+    
+    start = t.time()
+    #test_loss, test_acc = model.evaluate(test_ds, verbose=2)
+    _, test_acc = model.evaluate(test_ds, verbose=2)
+    end = t.time() - start
+    
+    model_path = f'{callback_folder_name}/saved_model.pb'
+    msize = os.path.getsize(model_path)
+    print(f'\nacc: {test_acc}, size: {msize/1024}kB Inference Latency {end}ms\n')
+else:
+    print(f'{callback_folder_name} already exists.')
+
+
+### Generating tflite models
+def generate_tflite(model_folder, output_name, mfccs=True):
+    #test_ds = tf.data.experimental.load(f'{dataset_dir}/th_test', tensor_specs)
+    if mfccs:
+        tensor_specs = (
+            tf.TensorSpec([None,49,10,1],dtype=tf.float32),
+            tf.TensorSpec([None,],dtype=tf.int64)
         )
-        
-        cp_callback = keras.callbacks.ModelCheckpoint(
-            callback_folder_name,
-            monitor = 'val_sparse_categorical_accuracy',
-            verbose = 0, 
-            save_best_only = True,
-            save_weights_only = False,
-            mode = 'auto',
-            save_freq = 'epoch'
+    else: # stft
+        tensor_specs = (
+            tf.TensorSpec([None,32,32,1],dtype=tf.float32),
+            tf.TensorSpec([None,],dtype=tf.int64)
         )
-        
-        model.fit(train_ds, batch_size=32, epochs=20, validation_data=val_ds,
-                callbacks=[cp_callback])
-        model.summary()
-        
-        start = t.time()
-        test_loss, test_acc = model.evaluate(test_ds, verbose=2)
-        end = t.time() - start
-        
-        msize = os.path.getsize(f'{callback_folder_name}/saved_model.pb')
-        print(f'\nacc: {test_acc}, size: {msize} Inference Latency {end}ms\n')
-    else:
-        print(f'{callback_folder_name} already exists.')
-
-
-# Saving basic tflite model
-test_ds = test_ds.unbatch().batch(1)
-tflite_dirs = './tflite_models'
-if not os.path.exists(tflite_dirs): 
-    os.mkdir(tflite_dirs)
-
-for model_name in models:
-    mod = models[model_name]
-    model_folder = f'./callback_test_chkp/{model_name}{"_mfccs" if mfccs else "_stft"}_chkp_best'
-    basic_file = f'{tflite_dirs}/{model_name}{"_mfccs" if mfccs else "_stft"}_basic.tflite'
-    optimized_file = f'{tflite_dirs}/{model_name}{"_mfccs" if mfccs else "_stft"}_optimized.tflite'
-    compressed_file = f'{tflite_dirs}/{model_name}{"_mfccs" if mfccs else "_stft"}_compressed.tflite.zlib'
+    
+    #train_ds = tf.data.experimental.load(f'{dataset_dir}/th_train', tensor_specs)
+    #val_ds = tf.data.experimental.load(f'{dataset_dir}/th_val', tensor_specs)
+    test_ds = tf.data.experimental.load(f'{dataset_dir}/th_test', tensor_specs)
+    
+    # Saving basic tflite model
+    test_ds = test_ds.unbatch().batch(1)
+    tflite_dirs = 'tflite_models'
+    if not os.path.exists(tflite_dirs): 
+        os.mkdir(tflite_dirs)
+    tflite_dirs = f'tflite_models/{output_name}'
+    
+    basic_file = f'{tflite_dirs}{"_mfccs" if mfccs else "_stft"}_basic.tflite'
+    optimized_file = f'{tflite_dirs}{"_mfccs" if mfccs else "_stft"}_optimized.tflite'
+    compressed_file = f'{tflite_dirs}{"_mfccs" if mfccs else "_stft"}_compressed.tflite.zlib'
     
     # Basic file
     if force_first_train or not os.path.exists(basic_file):
         with open(basic_file, 'wb') as f:
             converter = tf.lite.TFLiteConverter.from_saved_model(model_folder)
             tflite_model = converter.convert()
-            
+
             f.write(tflite_model)
     
     tflb_size = os.path.getsize(basic_file)
@@ -261,9 +269,9 @@ for model_name in models:
         with open(optimized_file, 'wb') as f:
             converter.optimizations = [tf.lite.Optimize.DEFAULT]
             tflite_quant_model = converter.convert()
-            
+
             f.write(tflite_quant_model)
-    
+
     tflo_size = os.path.getsize(optimized_file)
     
     # Compressed file
@@ -271,7 +279,7 @@ for model_name in models:
         with open(compressed_file, 'wb') as fp:
             tflite_compressed = zlib.compress(tflite_quant_model)
             fp.write(tflite_compressed)
-    
+
     tflc_size=os.path.getsize(compressed_file)
     
     
@@ -281,23 +289,17 @@ for model_name in models:
     
     # Get input and output tensors
     input_details = interpreter.get_input_details()
-    input_shape = input_details[0]['shape']
+    #input_shape = input_details[0]['shape']
     output_details = interpreter.get_output_details()
     
     num_corr = 0
     num = 0
     start = t.time()
     for input_data, label in test_ds:
-        #input_data = convert(input_data, 0, 255, np.uint8)
-        #label = convert(label, 0, 255, np.uint8)
-        #input_data = tf.quantization.quantize(input_data,min(input_data),max(input_data),tf.quint8)
-        #label = tf.quantization.quantize(label,min(LABELS),max(LABELS),tf.quint8)
-        
         interpreter.set_tensor(input_details[0]['index'], input_data)
         interpreter.invoke()
         output_data = np.argmax(interpreter.get_tensor(output_details[0]['index']))
-        #y_pred = tf.constant([output_data],dtype=tf.int64)
-        
+
         if label.numpy()[0] == output_data:
             num_corr += 1
         num += 1
@@ -310,93 +312,59 @@ for model_name in models:
     print(f'Compressed: {tflc_size/1024} kB')
     print(f'Accuracy: {num_corr/num}\nTime: {end} ms\n\n')
 
+generate_tflite(model_folder=callback_folder_name, output_name='not_pruned', mfccs=mfccs)
 
-# Stripping the model
+
+### Pruning models
 pruning_params = {
     'pruning_schedule' : tfmot.sparsity.keras.PolynomialDecay(
-        initial_sparsity=0.30, 
-        final_sparsity=0.8,
-        begin_step=len(train_ds)*5,
-        end_step=len(train_ds)*15
+        initial_sparsity = 0.3, 
+        final_sparsity = 0.8,
+        begin_step = len(train_ds) * 5,
+        end_step = len(train_ds) * 15
     )
 }
+
+# Stripping the model
 prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
+stripped_model_folder = f'./stripped/model{"_mfccs" if mfccs else "_stft"}_chkp_best'
 
-for model_name in models:
-    model_folder = f'./callback_test_chkp/{model_name}{"_mfccs" if mfccs else "_stft"}_chkp_best'
-    stripped_model_folder = f'./stripped/{model_name}{"_mfccs" if mfccs else "_stft"}_chkp_best'
+if force_first_train or not os.path.exists(stripped_model_folder):
+    model = keras.models.load_model(callback_folder_name)
+    model = prune_low_magnitude(model, **pruning_params)
     
-    if force_first_train or not os.path.exists(stripped_model_folder) or True:
-        model = tf.keras.models.load_model(model_folder)
-        model = prune_low_magnitude(model, **pruning_params)
-        
-        cp_callback = keras.callbacks.ModelCheckpoint(
-            f'./pruned/dscnn_chkp_best_mfccs',
-            monitor='val_loss',
-            verbose=0, 
-            save_best_only=True,
-            save_weights_only=False,
-            mode='auto',
-            save_freq='epoch'
-        )
-        callbacks = [tfmot.sparsity.keras.UpdatePruningStep(), cp_callback]
-        
-        input_shape = [32, 32, 32]
-        model.build(input_shape)
-        model.compile(
-            optimizer = 'Adam',
-            loss = keras.losses.SparseCategoricalCrossentropy(True),
-            metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
-        )
-        
-        model.fit(train_ds, epochs=20, validation_data=val_ds, callbacks=callbacks)
-        strip_model = tfmot.sparsity.keras.strip_pruning(model)
-        strip_model.save(stripped_model_folder)
-        strip_model.summary()
+    cp_callback = keras.callbacks.ModelCheckpoint(
+        f'./callback_test_chkp/model_pruned{"_mfccs" if mfccs else "_stft"}_chkp_best',
+        monitor='val_acc',
+        verbose=0, 
+        save_best_only=True,
+        save_weights_only=False,
+        mode='auto',
+        save_freq='epoch'
+    )
+    callbacks = [tfmot.sparsity.keras.UpdatePruningStep(), cp_callback]
     
-    """
-    # Evaluating the model
-    interpreter = tflite.Interpreter(model_path = stripped_model_folder)
-    interpreter.allocate_tensors()
+    if mfccs:
+        input_shape=[32,49,10]
+    else:
+        input_shape=[32,32,32]
     
-    # Get input and output tensors
-    input_details = interpreter.get_input_details()
-    input_shape = input_details[0]['shape']
-    output_details = interpreter.get_output_details()
+    model.build(input_shape)
+    model.compile(
+        optimizer = 'Adam',
+        loss = keras.losses.SparseCategoricalCrossentropy(True),
+        metrics = [keras.metrics.SparseCategoricalAccuracy()]
+    )
     
-    num_corr = 0
-    num = 0
-    start = t.time()
-    for input_data, label in test_ds:
-        interpreter.set_tensor(input_details[0]['index'], input_data)
-        interpreter.invoke()
-        output_data = np.argmax(interpreter.get_tensor(output_details[0]['index']))
-        
-        if label.numpy()[0] == output_data:
-            num_corr += 1
-        num += 1
-    end = t.time() - start
-    
-    msize = os.path.getsize(f'{stripped_model_folder}/saved_model.pb')
-    print(f'\nacc: {test_acc}, size: {msize} Inference Latency {end}ms\n')
-    
-    """
-    '''
-    start = t.time()
-    test_loss, test_acc2 = model.evaluate(test_ds, verbose=2)
-    end = t.time() - start
-    msize = os.path.getsize(f'./pruned/dscnn_chkp_best/saved_model.pb')
-    print()
-    print(f'acc: {test_acc2}, size: {msize} Inference Latency {end}ms')
-    print()
-    '''
+    model.fit(train_ds, epochs=20, validation_data=val_ds, callbacks=callbacks)
+    #model = keras.models.load_model(f'./pruned/dscnn_chkp_best_mfccs')
+    strip_model = tfmot.sparsity.keras.strip_pruning(model)
+    strip_model.save(stripped_model_folder)
+    strip_model.summary()
+else:
+    print(f'{stripped_model_folder} already exists.')
 
-
-
-
-
-
-
+generate_tflite(model_folder=stripped_model_folder, output_name="pruned", mfccs=mfccs)
 
 
 
