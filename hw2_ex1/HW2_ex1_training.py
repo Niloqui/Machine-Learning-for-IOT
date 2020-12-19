3b5a3b4d861ad34a187b18a5b3baccd63adb118b
usesnames <emanueledri@libero.it>
2020-12-19 14:45:17 +0100

    final version

--- hw2_ex1/HW2_ex1_training.py
+++ hw2_ex1/HW2_ex1_training.py
@@ -4,28 +4,25 @@ import os
 import pandas as pd
 import tensorflow as tf
 import tensorflow.lite as tflite
-import matplotlib.pyplot as plt
 import tensorflow_model_optimization as tfmot
 import zlib
 
-#from subprocess import call
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, required=True, help='model name', default = 'mlp')
-parser.add_argument('--labels', type=int, required=True, help='model output', default = 2)
-parser.add_argument('-outdir', type=str, help='model save output', default = "./hw2ex1/")
+parser.add_argument('--version', type=str, required=True, help='model version', default = 'a')
 args = parser.parse_args()
 
-alpha = 0.05
-final_sparsity = 0.20
-initial_sparsity = 0.10
+version = args.version
 
-output_folder = args.outdir
+alpha = 0.05
+#final_sparsity = 0.20
+#initial_sparsity = 0.10
+
+output_folder = f"./hw2ex1_{version}/"
 if not os.path.isdir(output_folder):
     os.mkdir(output_folder)
 else:
     #os.system(f"mkdir -r {output_folder}")
-    raise NameError('output folder already exists, choose another folder')
+    raise NameError('output folder already exists')
 
 seed = 42
 tf.random.set_seed(seed)
@@ -53,8 +50,7 @@ std = train_data.std(axis=0)
 
 label_width = 6
 input_width = 6
-LABEL_OPTIONS = args.labels
-MODEL_OPTIONS = args.model
+LABEL_OPTIONS = 2
 
 class WindowGenerator:
     def __init__(self, input_width, label_width, label_options, mean, std):
@@ -106,46 +102,24 @@ class WindowGenerator:
 
         return ds
         
-if LABEL_OPTIONS == 2:
-    num_labels = 2*label_width
-else:
-    num_labels = 1*label_width
 
-if MODEL_OPTIONS == 'mlp':
-    model = tf.keras.Sequential([
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(units=128*alpha, activation='relu'),
-        tf.keras.layers.Dense(units=128*alpha, activation='relu'),
-        tf.keras.layers.Dense(units=num_labels),
-        tf.keras.layers.Reshape((label_width,LABEL_OPTIONS), input_shape=(num_labels,))
-    ])
-        
-elif MODEL_OPTIONS == 'cnn-1d':
-    model = tf.keras.Sequential([
-        tf.keras.layers.Conv1D(filters=64*alpha, kernel_size=(3,), activation="relu"),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(units=64*alpha, activation='relu'),
-        tf.keras.layers.Dense(units=num_labels), 
-        tf.keras.layers.Reshape((label_width,LABEL_OPTIONS), input_shape=(num_labels,))
-    ])
-        
-elif MODEL_OPTIONS == 'lstm':
-    model = tf.keras.Sequential([
-        tf.keras.layers.LSTM(64*alpha),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(units=num_labels), 
-        tf.keras.layers.Reshape((label_width,LABEL_OPTIONS), input_shape=(num_labels,))
-    ])
+num_labels = 2*label_width
 
-else:
-    raise Exception("model not supported")
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv1D(filters=64*alpha, kernel_size=(3,), activation="relu"),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=64*alpha, activation='relu'),
+    tf.keras.layers.Dense(units=num_labels), 
+    tf.keras.layers.Reshape((label_width,LABEL_OPTIONS), input_shape=(num_labels,))
+])
+        
 
 class MultiOutputMAE(tf.keras.metrics.Metric):
     def __init__(self, name='mean_absolute_error', **kwargs):
         super().__init__(name, **kwargs)
         self.total = self.add_weight('total', initializer='zeros', shape=[2])
         self.count = self.add_weight('count', initializer='zeros')
-       
+    
     
     def reset_states(self):
         self.count.assign(tf.zeros_like(self.count))
@@ -170,38 +144,18 @@ train_ds = generator.make_dataset(train_data, True)
 val_ds = generator.make_dataset(val_data, False)
 test_ds = generator.make_dataset(test_data, False)
 
-"""cp_callback = tf.keras.callbacks.ModelCheckpoint(
-        f'./base/cnn_chkp_best',
-        monitor='val_mean_absolute_error',
-        verbose=0, 
-        save_best_only=True,
-        save_weights_only=False,
-        mode='auto',
-        save_freq='epoch'
-    )
-callbacks = [cp_callback]"""
-
 model.compile(optimizer='adam',loss=tf.keras.losses.MeanSquaredError(), metrics=[MultiOutputMAE()])
-history = model.fit(train_ds, epochs=20, validation_data=val_ds)#, callbacks=callbacks)
-error = model.evaluate(test_ds) #validation qui
-
-print("\n\n")
-print(error)
-#model.summary()
+history = model.fit(train_ds, epochs=20, validation_data=val_ds)
 
 run_model = tf.function(lambda x: model(x))
 concrete_func = run_model.get_concrete_function(tf.TensorSpec([1, 6, 2],
     tf.float32))
 tf.keras.models.save_model(model, output_folder, signatures=concrete_func)
 
-#tf.data.experimental.save(train_ds, './th_train')
-#tf.data.experimental.save(val_ds, './th_val')
-#tf.data.experimental.save(test_ds, './th_test')
-
-pruning_params = {'pruning_schedule':tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=initial_sparsity, 
-                                                                          final_sparsity=final_sparsity,
-                                                                          begin_step=len(train_ds)*5,
-                                                                          end_step=len(train_ds)*15)}
+"""pruning_params = {'pruning_schedule':tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=initial_sparsity, 
+                                                                        final_sparsity=final_sparsity,
+                                                                        begin_step=len(train_ds)*5,
+                                                                        end_step=len(train_ds)*15)}
 prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
 
 
@@ -221,32 +175,31 @@ model.build(input_shape)
 model.compile(optimizer='adam',loss=tf.keras.losses.MeanSquaredError(), metrics=[MultiOutputMAE()])
 model.fit(train_ds, epochs=20, validation_data=val_ds, callbacks=callbacks)
 strip_model = tfmot.sparsity.keras.strip_pruning(model)
-strip_model.save(f'./stripped/dscnn_chkp_best_mfccs')
+strip_model.save(f'./stripped/dscnn_chkp_best_mfccs')"""
 
-converter = tf.lite.TFLiteConverter.from_saved_model('./stripped/dscnn_chkp_best_mfccs')
+converter = tf.lite.TFLiteConverter.from_saved_model(output_folder)
 """WEIGHTS_ONLY QUANTIZATION"""
 converter.optimizations= [tf.lite.Optimize.DEFAULT]
 quant_model= converter.convert()
 
-with open('my_model.tflite', 'wb') as f:
+with open(f'Group2_th_{version}.tflite', 'wb') as f:
     f.write(quant_model)
     
-with open('Group2_th_a.tflite.zlib', 'wb') as fp:
+with open(f'Group2_th_{version}.tflite.zlib', 'wb') as fp:
     tflite_compressed = zlib.compress(quant_model)
     fp.write(tflite_compressed)
 
-tfl_size=os.path.getsize("my_model.tflite")
-tflc_size=os.path.getsize("Group2_th_a.tflite.zlib")
+tfl_size=os.path.getsize(f'Group2_th_{version}.tflite')
+tflc_size=os.path.getsize(f'Group2_th_{version}.tflite.zlib')
 
 
-interpreter = tf.lite.Interpreter(model_path='my_model.tflite')
+interpreter = tf.lite.Interpreter(model_path=f'Group2_th_{version}.tflite')
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 tensor_specs = (tf.TensorSpec([None,6,2], dtype=tf.float32),tf.TensorSpec([None,6,2]))
-#test_ds = tf.data.experimental.load('./th_test',tensor_specs)
 
 test_ds = test_ds.unbatch().batch(1)
 
@@ -264,6 +217,4 @@ mae_hum = sum_mae[0][1]/len(list(test_ds))
 print(f'size of optimized model: {tfl_size/1024} kB \ncompressed: {tflc_size/1024} kB')
 print("mae for temperature = ", mae_temp)
 print("mae for humidity = ", mae_hum)
-print(f'alpha = {alpha}, initial sparsity: {initial_sparsity}, final sparsity: {final_sparsity}')
-
-history.history 
+print(f'alpha = {alpha}, version = {version}')
