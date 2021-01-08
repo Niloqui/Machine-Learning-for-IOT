39434e644c20fa4dbf2999644533f60a05bfa76b
usesnames <56475319+usesnames@users.noreply.github.com>
2021-01-08 18:00:30 +0100

    initial commit

---
+++ hw3_ex1/big_service.py
@@ -0,0 +1,100 @@
+import base64
+import cherrypy
+import tensorflow as tf
+import json
+import numpy as np
+
+
+class InferenceService(object):
+    exposed = True
+
+    def __init__(self, model='./Group2_kws_c.tflite'):
+        self.model = model
+        if self.model is not None: 
+            self.interpreter = tf.lite.Interpreter(model_path=model)
+            self.interpreter.allocate_tensors()
+
+            self.input_details = self.interpreter.get_input_details()
+            output_details = self.interpreter.get_output_details()
+
+        labels_file = open("labels.txt", "r")
+        LABELS = labels_file.read()
+        labels_file.close()
+        self.labels = np.array(LABELS.split(" "))
+        
+        self.linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(40, 321, 16000, 20, 4000)
+
+    def preprocess(self, audio_bytes):
+        # Decode and normalize
+        audio, _ = tf.audio.decode_wav(audio_bytes)
+        audio = tf.squeeze(audio, axis=1)
+
+        # STFT
+        stft = tf.signal.stft(audio, frame_length=640, frame_step=320, fft_length=640)
+        spectrogram = tf.abs(stft)
+
+        # MFCC
+        mel_spectrogram = tf.tensordot(spectrogram, self.linear_to_mel_weight_matrix, 1)
+        log_mel_spectrogram = tf.math.log(mel_spectrogram + 1e-6)
+        mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)
+        mfccs = mfccs[..., :10]
+
+        # Add channel dimension
+        mfccs = tf.expand_dims(mfccs, -1)
+        # Add batch dimension
+        mfccs = tf.expand_dims(mfccs, 0)
+
+        return mfccs
+
+    def GET(self, *path, **query):
+        pass
+
+    def POST(self, *path, **query):
+        pass
+
+    def PUT(self, *path, **query):
+        input_body = cherrypy.request.body.read()
+        input_body = json.loads(input_body)
+        events = input_body['e']
+
+        audio_string = None
+        for event in events:
+            audio_string = event['audio']
+        if audio_string is None:
+            raise cherrypy.HTTPError(400, 'no audio event')
+
+        audio_bytes = base64.b64decode(audio_string)
+        mfccs = self.preprocess(audio_bytes)
+
+        model = self.model
+        if model is None:
+            raise cherrypy.HTTPError(400, 'no valid model')
+        
+        self.interpreter.set_tensor(input_details[0]['index'], mfcss)
+        #start_inf = time.time()
+        self.interpreter.invoke()
+        logits = self.interpreter.get_tensor(output_details[0]['index'])
+        #probs = tf.nn.softmax(logits)
+        #prob = tf.reduce_max(probs).numpy() * 100
+        label_idx = tf.argmax(logits, 1).numpy()[0]
+        label = self.labels[label_idx]
+
+        output_body = {
+                        'label': label,
+                        'probability': prob}
+        output_body = json.dumps(output_body)
+
+        return output_body
+
+    def DELETE(self, *path, **query):
+        pass
+
+if __name__ == '__main__':
+    conf = {'/': { 
+                'request.dispatch': cherrypy.dispatch.MethodDispatcher()
+                }} 
+    cherrypy.tree.mount(InferenceService(), '/', conf)
+    cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+    cherrypy.config.update({'server.socket_port': 8080})
+    cherrypy.engine.start()
+    cherrypy.engine.block() 
---
+++ hw3_ex1/little_client.py
@@ -0,0 +1,177 @@
+import argparse
+import numpy as np
+from subprocess import call
+import tensorflow as tf
+import time
+from scipy import signal
+import os
+import socket
+import requests
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--server_ip', type=str, default="192.168.1.54",
+        help='local adress o')
+parser.add_argument('--server_port', type=int, default=8079,
+        help='server port')		
+parser.add_argument('--rate', type=int, default=16000,
+        help='sampling rate after resampling')
+parser.add_argument('--mfcc', action='store_true',
+        help='use mfcc')
+parser.add_argument('--resize', type=int, default=32,
+        help='input size after resize')
+parser.add_argument('--length', type=int, default=640,
+        help='stft window legnth in number of samples')
+parser.add_argument('--stride', type=int, default=320,
+        help='stft window stride in number of samples')
+parser.add_argument('--bins', type=int, default=40,
+        help='number of mel bins')
+parser.add_argument('--coeff', type=int, default=10,
+        help='number of MFCCs')
+parser.add_argument('--th', type=float, default=0.5,
+        help='threshold for score margin')
+args = parser.parse_args()
+
+
+call('sudo sh -c "echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"',
+            shell=True)
+
+rate = args.rate
+length = args.length
+stride = args.stride
+resize = args.resize
+num_mel_bins = args.bins
+num_coefficients = args.coeff
+threshold = args.th
+
+data_dir = os.path.join('.','data', 'mini_speech_commands')
+if not os.path.exists(data_dir):
+    zip_path = tf.keras.utils.get_file(
+        origin='http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip',
+        fname='mini_speech_commands.zip',
+        extract=True,
+        cache_dir='.', cache_subdir='data')
+
+# raspberry ip 
+RASPIP = '198.168.1.60'
+
+model = './Group2_kws_c.tflite' # load small model
+
+num_frames = (rate - length) // stride + 1
+num_spectrogram_bins = length // 2 + 1
+
+linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
+        num_mel_bins, num_spectrogram_bins, rate, 20, 4000)
+
+if model is not None:
+    interpreter = tf.lite.Interpreter(model_path=model)
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+f = open("kws_test_split.txt", "r")
+test_set = f.readlines()
+f.close()
+
+data_dir = os.path.join('.','data', 'mini_speech_commands')
+fl = open("labels.txt", "r") 
+LABELS = fl.readlines()
+fl.close()
+
+
+inf_latency = []
+tot_latency = []
+num = 0
+num_corr = 0
+for audio_path in test_set:
+    num += 1
+    if num%100 == 0:
+        print(num)
+    
+    #sample = np.array(np.random.random_sample(48000), dtype=np.float32)
+    parts = tf.strings.split(audio_path, os.path.sep)
+    label = parts[-2]
+    label = tf.argmax(label == LABELS)
+    label = label.numpy()
+    
+    audio_binary = tf.io.read_file(audio_path.replace("\n",''))
+    audio_raw, _ = tf.audio.decode_wav(audio_binary)
+    audio = tf.squeeze(audio_raw, axis=1)
+    zero_padding = tf.zeros([16000] - tf.shape(audio), dtype=tf.float32)
+    audio = tf.concat([audio,zero_padding],0)
+    sample = audio.numpy()
+    
+    start = time.time()
+
+    # Resampling
+    #sample = signal.resample_poly(sample, 1, 48000 // rate)
+    sample = signal.resample_poly(sample, 1, 16000 // rate)
+    sample = tf.convert_to_tensor(sample, dtype=tf.float32)
+
+    # STFT
+    stft = tf.signal.stft(sample, length, stride,
+            fft_length=length)
+    spectrogram = tf.abs(stft)
+    
+    
+    if args.mfcc is False and args.resize > 0:
+        # Resize (optional)
+        spectrogram = tf.reshape(spectrogram, [1, num_frames, num_spectrogram_bins, 1])
+        spectrogram = tf.image.resize(spectrogram, [resize, resize])
+        input_tensor = spectrogram
+    else:
+        # MFCC (optional)
+        mel_spectrogram = tf.tensordot(spectrogram, linear_to_mel_weight_matrix, 1)
+        log_mel_spectrogram = tf.math.log(mel_spectrogram + 1.e-6)
+        mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)
+        mfccs = mfccs[..., :num_coefficients]
+        mfccs = tf.reshape(mfccs, [1, num_frames, num_coefficients, 1])
+        input_tensor = mfccs
+    
+
+    interpreter.set_tensor(input_details[0]['index'], input_tensor)
+    start_inf = time.time()
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+
+    if (np.sort(output_data)[0] - np.sort(output_data)[1]) <= threshold:
+
+        body = {
+                'bn': f'http://{RASPIP}/',
+                'e': [{'audio': audio_raw}]
+                }
+
+        url = 'http://{server_ip}:{server_port}/'
+        r = requests.put(url, json=body)
+
+        if r.status_code == 200:
+            rbody = r.json()
+            #prob = rbody['probability']
+            label = rbody['label']
+            
+        else:
+            print('Error')
+            print(r.text)	
+    
+    net_cost = len(request.content) + len(response.content)
+    if label == np.argmax(output_data):
+        num_corr += 1
+
+    end = time.time()
+    tot_latency.append(end - start)
+
+    if args.model is None:
+        start_inf = end
+
+    inf_latency.append(end - start_inf)
+    #time.sleep(0.1)
+    
+print('Inference Latency {:.2f}ms'.format(np.mean(inf_latency)*1000.))
+print('Total Latency {:.2f}ms'.format(np.mean(tot_latency)*1000.))
+print(f'Communication cost {net_cost/(2014*8)}')
+print('Accuracy', num_corr/num)
+
+
+#from subprocess import check_output
+#print check_output(['hostname', '-I'])
