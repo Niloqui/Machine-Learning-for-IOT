b09ed28d46572ab29db13cadb3dc11d0a4a37eb1
Alessandro Nicolì <38593853+Niloqui@users.noreply.github.com>
2021-01-20 16:20:58 +0100

    Update cooperative_client.py

--- Homework_3/hw3_ex2/cooperative_client.py
+++ Homework_3/hw3_ex2/cooperative_client.py
@@ -6,7 +6,6 @@ import time as t
 import json
 import numpy as np
 import pandas as pd
-import tensorflow_model_optimization as tfmot
 import tensorflow as tf
 import datetime
 from scipy import signal
